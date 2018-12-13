01f37f011dd380c67e45d58a42d3e7ad21d96b41
Chris Li <chris@kiwix.org>
2018-12-12 20:26:49 -0500

    credentials

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from zimfarm.models import Worker
 
 
 if __name__ == '__main__':
-
+    username = ''
+    password = ''
 
     zimfarm.login(username, password)
     workers = Worker.list()
     print(workers)
-
+    print([worker.last_heartbeat for worker in workers])
